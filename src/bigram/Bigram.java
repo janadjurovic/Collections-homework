767da6a0ea9afd00fe15e8acb87890eb0df1b5cc
janadjurovic <djurovic.jana@outlook.com>
2016-06-14 23:37:12 +0200

    Zavrsen projekat

--- src/bigram/Bigram.java
+++ src/bigram/Bigram.java
@@ -1,6 +1,7 @@
 package bigram;
 
 import java.util.Map.Entry;
+import java.util.Scanner;
 import java.util.HashMap;
 
 public class Bigram {
@@ -8,20 +9,36 @@ public class Bigram {
 	public static void main(String[] args) {
 		HashMap<String, Integer> mapaFrekvencija = new HashMap<>();
 		String bigram = "abbcceeeeeeabcc";
-		
+		Scanner in = new Scanner(System.in);
+
 		for (int i = 1; i < bigram.length(); i++) {
-			if(mapaFrekvencija.containsKey(bigram.substring(i-1, i+1))){
-				mapaFrekvencija.replace(bigram.substring(i-1, i + 1), mapaFrekvencija.get(bigram.substring(i-1, i + 1)) + 1);
+			if (mapaFrekvencija.containsKey(bigram.substring(i - 1, i + 1))) {
+				mapaFrekvencija.replace(bigram.substring(i - 1, i + 1),
+						mapaFrekvencija.get(bigram.substring(i - 1, i + 1)) + 1);
+			} else {
+				mapaFrekvencija.put(bigram.substring(i - 1, i + 1), 1);
 			}
-			else{
-				mapaFrekvencija.put(bigram.substring(i-1, i+1), 1);
-			}
-			
-		}
-		for(Entry<String, Integer> entry : mapaFrekvencija.entrySet()){
-			System.out.println(entry.getKey() + " " + entry.getValue());
+
 		}
 
+		for (Entry<String, Integer> entry : mapaFrekvencija.entrySet()) {
+			System.out.println(entry.getKey() + " - " + entry.getValue());
+		}
+
+		System.out.println("Unesite bigram : \n");
+		String unos = in.nextLine();
+		char c = ' ';
+		for (int i = 0; i < 3; i++) {
+			int pom = 0;
+			for (Entry<String, Integer> e : mapaFrekvencija.entrySet()) {
+				if (e.getKey().charAt(0) == unos.charAt(unos.length() - 1) && e.getValue() > pom) {
+					c = e.getKey().charAt(1);
+					pom = e.getValue();
+				}
+			}
+			unos += c;
+		}
+		System.out.println(unos);
 	}
 
 }
